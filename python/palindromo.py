ba93756f653e54e8b9dc13305eeb470d172d1e85
Emmanuel Luján <lujan.emmanuel@gmail.com>
2019-04-06 16:31:16 -0300

    palindrome

---
+++ python/palindromo.py
@@ -0,0 +1,25 @@
+# Return if a string is a palindrome
+
+def palindrome(s):
+	n = len(s)
+	i = 0
+	while i <= n/2  and s[i] == s[n-1-i]:
+		i += 1
+	if i > n/2:
+		return True
+	else:
+		return False
+
+print(palindrome("a"))
+print(palindrome("aa"))
+print(palindrome("aba"))
+print(palindrome("abccba"))
+print(palindrome("abcdcba"))
+
+print(palindrome("ab"))
+print(palindrome("abb"))
+print(palindrome("abcdba"))
+print(palindrome("abcdecba"))
+
+
+
